449c6aac5840e2a9cbd0309c30f6f492b9ab6e18
ongks <ongkashing@gmail.com>
2017-04-17 00:17:43 +0800

    Add callback function that connects to GitHub API

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,28 @@ router.get('/search', function(req, res) {
 	var url = 'https://api.github.com/legacy/repos/search/' + val;
 	console.log(url);
 
+	//headers required to access github api
+	var options = {
+		url: url,
+		headers: {
+			'User-Agent': 'ongks'
+		}
+	};
+
+	function callback(err, resp, body) {
+		body = JSON.parse(body);
+		// logic used to compare search results with the input from user
+		if (!body['repositories']) {
+			searchRes = "No results found. Try again.";
+		} else {
+			searchRes = body['repositories'];
+		}
+		// pass back the results to client side
+		res.send(searchRes);
+	}
+
+	// request module is used to process the github api url and return the results in JSON format
+	request(options, callback);
 });
 
 module.exports = router;
