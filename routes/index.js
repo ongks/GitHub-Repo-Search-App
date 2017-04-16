801ef037ef5fec2b32e8b8d6eb0c54c438561603
ongks <ongkashing@gmail.com>
2017-04-16 23:12:37 +0800

    Create basic UI elements

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'GitHub Search' });
+});
+
+router.get('/search', function(req, res) {
+
 });
 
 module.exports = router;
