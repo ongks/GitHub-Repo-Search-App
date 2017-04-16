ea768ede75a584a396b27f4905cedd5768a64728
ongks <ongkashing@gmail.com>
2017-04-17 00:54:24 +0800

    update main.js to interpret presence or absence of data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,17 @@
 //Captures search result in 'val' upon pressing enter
 $(function(){
- $('#search').on('keyup', function(e){
-   if(e.keyCode === 13) {
-     var parameters = { search: $(this).val() };
-       $.get( '/search',parameters, function(data) {
-       $('#results').html(data);
-     });
-    };
- });
+    $('#search').on('keyup', function(e){
+        if(e.keyCode === 13) {
+            var parameters = { 
+                search: $(this).val() 
+            };
+            $.get( '/search',parameters, function(data) {
+                if (data instanceof Array) {
+                    $results.html(dataTemplate({resultsArray: data}));
+                } else {
+                    $results.html(data);
+                }
+            });
+        };
+    });
 });
